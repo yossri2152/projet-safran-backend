20cb203678184c6da2655e3964263291186964a5
Yossri Amri <yossriamri2222@gmail.com>
2025-06-15 10:04:58 +0100

    chore: switch from bcrypt to bcryptjs for Render deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User"); // adapte le chemin si nécessaire
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const { sendPasswordResetEmail } = require('../utils/emailService');
 
